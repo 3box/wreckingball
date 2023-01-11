c73f6d434ed03973b8876b876aa0877b28923678
Ben Wilson <ben@3box.io>
2023-01-11 12:14:33 -0500

    Added multiple readers and lambda timeout handling

--- src/handlers/composedb.ts
+++ src/handlers/composedb.ts
@@ -47,7 +47,8 @@ export async function consumer(event: SQSEvent) {
     );
 
     console.log("r", record);
-    const body = JSON.parse(record.body);
+    let body: Task = JSON.parse(record.body);
+    console.log("parsing passed", body);
     // Case crete new doc
     if (!('streamId' in body)) {
       const seed = randomBytes(32);
@@ -65,28 +66,40 @@ export async function consumer(event: SQSEvent) {
       });
       console.log(`Created streamId: `, doc.id.toString());
       console.log(doc.state);
-      sqsPromises.push(
-        sqs
-          .sendMessage({
-            QueueUrl: QUEUE_URL,
-            MessageBody: JSON.stringify(messageBody),
-          })
-          .promise()
-      );
+      for (let step = 0; step < body.numberOfReaders; step++) {
+        sqsPromises.push(
+          sqs
+            .sendMessage({
+              QueueUrl: QUEUE_URL,
+              MessageBody: JSON.stringify(messageBody),
+            })
+            .promise()
+        );
+      }
       console.log(await cloudwatch.putMetricData(createDocMetric(body.identifier)).promise())
 
     } else if ('streamId' in body && body.jobRunReadSeconds > 0) {
       // Case read the doc
-      const readLoopStart = Date.now();
       console.log(`msLeft: `, (body.jobRunReadSeconds * 1000) - (Date.now() - body.jobStartTimestamp));
 
       while ((body.jobRunReadSeconds * 1000) - (Date.now() - body.jobStartTimestamp) > 0) {
-        // if ((lambdaRuntimeSeconds * 1000) - (readLoopStart - Date.now()) > 0) {
-        //   console.log(`Lambda runtime reached: `, body.streamId);
-        //   return "success";
-        // }
-        
-        console.log(`msLeft: `, (body.jobRunReadSeconds * 1000) - (Date.now() - body.jobStartTimestamp));
+        console.log(`lambdaRuntime left : `, (lambdaRuntimeSeconds * 1000 * body.generation) - (Date.now() - body.jobStartTimestamp));
+
+        if ((Date.now() - body.jobStartTimestamp) > (lambdaRuntimeSeconds * 1000 * body.generation)) {
+          body.generation += 1; // TODO: this is showing up as generation 4 on the first iteration?!? 
+          console.log(`Lamdba timeout almost reached, resubmitting`);
+          sqsPromises.push(
+            sqs
+              .sendMessage({
+                QueueUrl: QUEUE_URL,
+                MessageBody: JSON.stringify(body),
+              })
+              .promise()
+          );
+          return "lambda timeout"
+        }
+        const msLeft = (body.jobRunReadSeconds * 1000) - (Date.now() - body.jobStartTimestamp);
+        console.log(`msLeft: `, msLeft);
         const seed = uint8arrays.fromString(body.seed, "base64url");
         const ceramic = await createCeramic(body.endpoint, seed);
         const doc = await ModelInstanceDocument.load(ceramic, body.streamId);
@@ -157,13 +170,15 @@ export async function trigger(event: APIGatewayEvent) {
 
     const state = 'Starting';
     const numberOfDocs = body.numberOfDocs || 1;
+    const numberOfReaders = body.numberOfReaders || 1;
     const jobRunReadSeconds = body.jobRunReadSeconds || 60;
     const jobRunUpdateSeconds = body.jobRunUpdateSeconds || 0;
-    const jobStartTimestamp = Date.now()
+    const jobStartTimestamp = Date.now();
+    const generation = 1;
     const identifier =
       body.identifier || `composedb-run-${Math.floor(Math.random() * 100000)}`;
 
-    const messageBody = JSON.stringify({ state, identifier, endpoint, numberOfDocs, jobRunReadSeconds, jobRunUpdateSeconds, jobStartTimestamp });
+    const messageBody = JSON.stringify({ state, generation, identifier, endpoint, numberOfDocs, numberOfReaders, jobRunReadSeconds, jobRunUpdateSeconds, jobStartTimestamp });
     console.log("queue_url", process.env.QUEUE_URL);
     const promises = Array.from({ length: numberOfDocs }).map((_, index) => {
       return sqs
@@ -194,4 +209,17 @@ export async function trigger(event: APIGatewayEvent) {
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+interface Task {
+  state: string;
+  generation: number;
+  seed: string;
+  identifier: string;
+  endpoint: string;
+  numberOfDocs: number;
+  numberOfReaders: number;
+  jobRunReadSeconds: number;
+  jobRunUpdateSeconds: number;
+  jobStartTimestamp: number;
 }
